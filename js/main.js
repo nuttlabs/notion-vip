bf51106340b66ad949bc37b55543ec80d4c931f9
nuttlabs <122705994+nuttlabs@users.noreply.github.com>
2023-01-30 12:30:59 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', () => {
 	addVersionToStylesheets();
+	replaceNebulaGradients();
 	addGradientTextClass();
 });
 
@@ -13,6 +14,16 @@ function addGradientTextClass() {
 	document.querySelectorAll('h1 span').forEach( (span) => {
 		if( span.getAttribute('style') && span.getAttribute('style').match('background-clip') ) {
 			span.classList.add('paddedGradientText');
+		}
+	});
+
+}
+
+function replaceNebulaGradients() {
+
+	document.querySelectorAll('span').forEach( span => {
+		if( span.style.backgroundImage == "linear-gradient(135deg, rgb(102, 153, 255) 0%, rgb(255, 51, 102) 100%)" ) {
+			span.style.backgroundImage = "var(--gradient--nVip)";
 		}
 	});
 
