557d9a7442e3e275f2b8ce0de7e1b582a6fbc08f
nuttlabs <122705994+nuttlabs@users.noreply.github.com>
2023-02-19 19:04:18 -0500

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+window.addEventListener('load', () => {
+    freezeBodyOnMenuOpen();
+});
+
+function freezeBodyOnMenuOpen() {
+
+    let menu = document.querySelector('.c-header__menu');
+
+    let observer = new MutationObserver( (mutationList, observer) => {
+        if( document.querySelector('.c-header__menu').style.display == 'flex' ){
+            document.body.style.position = 'fixed';
+        } else {
+            document.body.style.position = 'static';
+        }
+    });
+
+    observer.observe(menu, {attributes: true});
+
+}
