284ddd829cc9d3f4f621457b7f58187f9cf9274d
nuttlabs <122705994+nuttlabs@users.noreply.github.com>
2023-03-04 18:47:31 -0500

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,18 +1,32 @@
+document.onload = () => {
+
+	configureFormHandlers();
+
+}
+
+function configureFormHandlers() {
+
+	let form = document.getElementById('subscriptionForm');
+
+	form.subscribeBtn.setAttribute('type', 'button');
+
+}
+
 function handleSubscription(event) {
 
 	let form = event.target;
 
 	console.log(form);
 
-	return false;
-
-	let enteredEmail = subscribeForm.email.value;
+	let enteredEmail = form.email.value;
 
 	let emailPattern = /.+@[a-z0-9]{2,}\.[a-z]{2,}/i;
 
 	let isValidEmail = enteredEmail.match(emailPattern) != null;
 
 	console.log(isValidEmail);
+
+	return false;
 
 	if( isValidEmail ) {
 		console.log("Bad email!");
