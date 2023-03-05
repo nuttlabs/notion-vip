b11577fa82de0247d3c2f3d8224f4c8ba04ab187
nuttlabs <122705994+nuttlabs@users.noreply.github.com>
2023-03-05 12:07:26 -0500

    Update home.js

--- js/home.js
+++ js/home.js
@@ -49,11 +49,11 @@ function sendToMailerLite(name, email) {
 			'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiNGQwZWExZWJjZjRjMDE3NjNkMWVmOWVjMTk4NmY3NTFiMDA2MTFkNDc2ZGJiMTdiM2UwZGM2OTVhZjNjMTAwOTRhZDdkY2MzNjE5ZDYzYTkiLCJpYXQiOjE2Nzc5ODAzNTUuMDU3NTY2LCJuYmYiOjE2Nzc5ODAzNTUuMDU3NTcsImV4cCI6NDgzMzY1Mzk1NS4wNTAzNzksInN1YiI6IjE4NDQiLCJzY29wZXMiOltdfQ.wLzwXZfLjgAon_gH9ZnGRkH0k5L9Qnks54U7UEYv8Nl610B7xbX80XSE7FMwhveUN-65YVDVRXNV4lS3Sx6XN95JSfvD2Qbe-IS7U2qWopNYYIjeylBW4iPz5ik2nnxRLCeTXcfoaLEyyMKaibvguqujTNZp2MM2SLU2TwCbfymAn5kntxyagV7k1qIGVS7rlehb-ysDh3J5TV-iGZo6sm2O3-s5_wX1btjicAtD9BhROc0yz5sv-XmTx6VSQvYRwEAzly1JV5q9fdpO9tRU4An3poa4ZnMOX4izL6PcRTk7x1tU4kRNQ0_QgEv9Ypg8wb-Q430N0jjizsxRM6Ibu0CrKrRDKT8w9Pnj-bYwnmJZGqX1sci-hk4cmzHwL776v5Iv-hHnmkeehen3-rjgwNSzV_jzpnTWLvjrBZTJxqtE6xk5Qzep7rIzUKPt16vmh8BN07AjxHQk-gUahhVqHgMJCafvT2boMdKvEwBFDlDwglbIKBewDoBYazaH_AFWlcGMT4a8fFM6hVsoL9yHa_1tRwdvcqKZ17DVjcSNn5o-TqpSxpji3f6BPVJbubQ80wr9IiaIhcjKIdrexsWBd_eWY_99uO04Ts7nv8Wu_z-NF_gJfV8RgX27Gyp814F8lqRUP2C6EkGK3Fowvc5KEUEPnb33-uCsAxN82LFDsNs'
 		},
 		body: `{
-			email: ${email},
-			fields: {
-				"First Name": ${name}
+			"email": "${email}",
+			"fields": {
+				"First Name": "${name}"
 			},
-			groups: ["81656172659082806"]
+			"groups": ["81656172659082806"]
 		}`
 	}
 
