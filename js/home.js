a171de4fb4c46da70a2e8cc73ae432e1240747a5
nuttlabs <122705994+nuttlabs@users.noreply.github.com>
2023-03-04 19:59:35 -0500

    Update home.js

--- js/home.js
+++ js/home.js
@@ -18,14 +18,18 @@ function configureFormHandlers() {
 
 function handleSubscription(form) {
 
-	console.log("Processing form.");
-
 	let enteredEmail = form.email.value;
 
 	let emailPattern = /.+@[a-z0-9]{2,}\.[a-z]{2,}/i;
 
 	let isValidEmail = emailPattern.test(enteredEmail);
 
-	console.log(isValidEmail);
+	if( ! isValidEmail ) {
+		let emailValidationMessage = document.querySelector('.p-home__form__emailInvalid');
+		emailValidationMessage.style.display = 'block';
+		emailValidationMessage.style.opacity = 1;
+	} else {
+		form.submit();
+	}
 
 }
