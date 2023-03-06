2ceab683c7dcb041de96daef41c8815230f509f4
nuttlabs <122705994+nuttlabs@users.noreply.github.com>
2023-03-06 15:17:53 -0500

    Create home.js

---
+++ js/home.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', addButtonListener );
+
+function addButtonListener() {
+
+	let subscribeBtn  = document.getElementById('subscribeBtn');
+
+	subscribeBtn.onclick = advanceToPart2;
+
+}
+
+function advanceToPart2() {
+
+	let part1 = document.getElementById('formPart1');
+	let part2 = document.getElementById('formPart2');
+
+	part1.style.opacity = '0';
+
+	setTimeout( () => {
+
+		part1.style.display = 'none';
+		part2.style.display = 'block';
+
+		setTimeout( () => {
+			part2.style.opacity = '0';
+		}, 200);
+
+	}, 200);
+
+}
